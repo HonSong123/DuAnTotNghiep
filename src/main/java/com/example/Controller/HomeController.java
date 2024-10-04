dc348762c434a475ce7aefd4350d368b726d78dc
HonSong123 <sonn59298@gmail.com>
2024-10-04 16:17:39 +0700

    sửa lại

--- src/main/java/com/example/Controller/HomeController.java
+++ src/main/java/com/example/Controller/HomeController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class HomeController {
     @GetMapping("/")
     public String index(){
-        return "index";
+        return "/index";
     }
 
     // @RequestMapping("/admin/home/index")
